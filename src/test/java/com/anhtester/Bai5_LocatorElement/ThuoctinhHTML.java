8c90232ad16670965f6663e2d67b82b5b72d9fef
JunVu <kimdungcnc51.neu@gmail.com>
2024-10-01 01:30:46 +0700

    Thực hành Xpath tương đối

--- src/test/java/com/anhtester/Bai5_LocatorElement/ThuoctinhHTML.java
+++ src/test/java/com/anhtester/Bai5_LocatorElement/ThuoctinhHTML.java
@@ -14,17 +14,23 @@ public class ThuoctinhHTML {
         WebDriver driver;
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //Thời gian chờ đợi tối đa để tiìm kiếm element
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //Thời gian chờ đợi tối đa để tìm kiếm element
 
         //Code Auto thao tác
         driver.get("https://crm.anhtester.com/admin/authentication");
         Thread.sleep(1000); //Khoảng ngừng giữa các thao tác
         driver.findElement(By.id("email")).sendKeys("admin@example.com");//Tìm kiếm element
+//        driver.findElement(By.name("email")).sendKeys("admin@example.com"); // Tìm theo name.
+
         driver.findElement(By.name("password")).sendKeys("123456");
-//        driver.findElement(By.linkText("Forgot Password?")).click();
-//        driver.findElement(By.partialLinkText("Forgot")).click();
+//        driver.findElement(By.id("password")).sendKeys("123456");// Tìm theo ID
+
+//        driver.findElement(By.linkText("Forgot Password?")).click(); //Tìm theo linkText
+//        driver.findElement(By.partialLinkText("Forgot")).click();  //Tìm theo 1 phần linkText
+
         driver.findElement(By.className("btn")).click(); //className dễ bị trùng nên ít dùng. class="btn btn-primary btn-block" (gồm 3 tên), Selenium chỉ nhận 1 tên trong 3 tên
         driver.findElement(By.tagName("button")).click();// tagName dễ bị trùng nên ít dùng
+
         Thread.sleep(1000);
         driver.findElement(By.id("search_input")).sendKeys("Selenium");
         Thread.sleep(2000);
